e579f645d3f3cdbbc9d3a9d0423d74284b5ed77c
Vinicius Roriz <78430207+viniciusroriz@users.noreply.github.com>
2022-10-13 11:31:47 -0300

    Add files via upload

--- scripts/install_apps.ps1
+++ scripts/install_apps.ps1
@@ -3,11 +3,6 @@ Write-Output ""
 
 Set-ExecutionPolicy AllSigned
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-Write-Output ""
-Write-Output "Configuring Chocolatey --------------------------------------------------"
-Write-Output ""
-
 choco feature enable -n allowGlobalConfirmation
 
 Write-Output ""
@@ -15,20 +10,22 @@ Write-Output "Installing Programs ----------------------------------------------
 Write-Output ""
 
 # dev
-choco install python3 -y
-choco install git -y
-choco install vscode -y
-choco install oraclejdk -y
-choco install javaruntime -y
+choco install python3
+choco install git
+choco install vscode
+choco install oraclejdk
+choco install javaruntime
 
 # utility
-choco install 7zip.install  -y
-choco install vlc -y
-choco install firefox -y
-choco install lightshot -y
-choco install notepadplusplus.install -y
-choco install qbittorrent -y
+choco install 7zip.install
+choco install vlc
+choco install firefox
+choco install lightshot
+choco install notepadplusplus.install
+choco install qbittorrent
 
 # games
-choco install steam-client -y
-choco install discord.install -y
+choco install steam-client
+choco install discord.install
+
+choco feature disable -n allowGlobalConfirmation
